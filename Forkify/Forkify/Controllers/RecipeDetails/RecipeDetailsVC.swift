90b3fe709ee86f1d44a6e172188253317b571995
Islam Nabil <islamelgaafary@gmail.com>
2021-05-19 18:34:03 +0200

    Add RecipeDetails UI

---
+++ Forkify/Forkify/Controllers/RecipeDetails/RecipeDetailsVC.swift
@@ -0,0 +1,26 @@
+//
+//  RecipeDetailsVC.swift
+//  Forkify
+//
+//  Created by Islam Elgaafary on 19/05/2021.
+//
+
+import UIKit
+
+class RecipeDetailsVC: UIViewController {
+
+    @IBOutlet weak var recipeImage: UIImageView!
+    @IBOutlet weak var recipeTitleLabel: UILabel!
+    @IBOutlet weak var ingredientsTableView: UITableView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    @IBAction func visitWebsitePressed(_ sender: Any) {
+    }
+    
+}
