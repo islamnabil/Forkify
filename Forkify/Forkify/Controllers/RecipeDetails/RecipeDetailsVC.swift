cd931e8d63a18cec74c1643208c0a52bcb79d8ff
Islam Nabil <islamelgaafary@gmail.com>
2021-05-19 18:54:40 +0200

    Add IngredientTableCell UI

--- Forkify/Forkify/Controllers/RecipeDetails/RecipeDetailsVC.swift
+++ Forkify/Forkify/Controllers/RecipeDetails/RecipeDetailsVC.swift
@@ -8,7 +8,9 @@
 import UIKit
 
 class RecipeDetailsVC: UIViewController {
-
+    
+    let api:HomeAPIProtocol = HomeAPI()
+    
     @IBOutlet weak var recipeImage: UIImageView!
     @IBOutlet weak var recipeTitleLabel: UILabel!
     @IBOutlet weak var ingredientsTableView: UITableView!
@@ -16,7 +18,6 @@ class RecipeDetailsVC: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
     }
     
 
@@ -25,3 +26,4 @@ class RecipeDetailsVC: UIViewController {
     }
     
 }
+
---
+++ Forkify/Forkify/Views/IngredientTableCell/IngredientTableCell.swift
@@ -0,0 +1,30 @@
+//
+//  IngredientTableCell.swift
+//  Forkify
+//
+//  Created by Islam Elgaafary on 19/05/2021.
+//
+
+import UIKit
+
+class IngredientTableCell: UITableViewCell {
+
+    
+    @IBOutlet weak var ingredientLabel: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+    func configureView(ingredient:String) {
+        ingredientLabel.text = ingredient
+    }
+    
+}
