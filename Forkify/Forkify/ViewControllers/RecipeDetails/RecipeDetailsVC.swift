cfbb33c55f0e4f35c8b3a791b1da5856134016ba
Islam Nabil <islamelgaafary@gmail.com>
2021-05-21 19:39:03 +0200

    Update IngredientTableCell Heights to be dynamic to its contents

--- Forkify/Forkify/ViewControllers/RecipeDetails/RecipeDetailsVC.swift
+++ Forkify/Forkify/ViewControllers/RecipeDetails/RecipeDetailsVC.swift
@@ -68,9 +68,6 @@ extension RecipeDetailsVC: UITableViewDataSource {
         return cell
     }
     
-    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
-        return 80
-    }
     
 }
 
